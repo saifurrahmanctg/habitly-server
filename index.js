43f3edf49067a725b53049e6144c93fd5ea1cbd0
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2025-11-09 12:38:13 +0600

    Install and configure Express.js, Setup MongoDB connection

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://habitlyDB:S2oLp9Rtgmlh5Oio@cluster0.vnhtgsv.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running, Fine!");
